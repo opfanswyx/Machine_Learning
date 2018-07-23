f5d84dfb58d6f794efa260321ffe1dd54411b796
e <opfanswyx@outlook.com>
2018-07-23 13:21:01 +0800

    commint

---
+++ Sklearn/Unsupervised_Learning/DBSCAN.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+import numpy as np
+from sklearn.cluster import DBSCAN
+
+import sklearn.cluster as skc
+from sklearn import metrics
+import matplotlib.pyplot as plt
+
+mac2id=dict()
+onlinetimes=[]
+f=open('TestData.txt',encoding='gb18030', errors='ignore')
+for line in f:
+    mac=line.split(',')[2]
+    onlinetime=int(line.split(',')[6])
+    starttime=int(line.split(',')[4].split(' ')[1].split(':')[0])
+    if mac not in mac2id:
+        mac2id[mac]=len(onlinetimes)
+        onlinetimes.append((starttime,onlinetime))
+    else:
+        onlinetimes[mac2id[mac]]=[(starttime,onlinetime)]
+
+real_x=np.array(onlinetimes).reshape((-1,2))
+
+X=real_x[:,0:1]
+
+
+db=skc.DBSCAN(eps=0.01,min_samples=20).fit(X)
+labels=db.labels_
+
+print('Labels:')
+print(labels)
+raito=len(labels[labels[:]==-1])/len(labels)
+print('Noise raito:',format(raito,'.2%'))
+
+n_clusters_=len(set(labels))-(1 if -1 in labels else 0)
+
+print('Estimated number of clusters: %d'% n_clusters_)
+print("Silhouette Coefficient: %0.3f"% metrics.silhouette_score(X,labels))
+
+for i in range(n_clusters_):
+    print('Cluster',i,':')
+    print(list(X[labels==i].flatten()))
+
+plt.hist(X,24)
+plt.show()
+
+
